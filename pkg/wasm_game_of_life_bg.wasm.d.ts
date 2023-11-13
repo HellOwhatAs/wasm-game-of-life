50f79379134eef3a5d5a29a038afa4e8bf0ec07a
HellOwhatAs <xjq701229@outlook.com>
2023-11-13 22:59:13 +0800

    commit

---
+++ pkg/wasm_game_of_life_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(): void;
+export function __wbg_universe_free(a: number): void;
+export function universe_tick(a: number): void;
+export function universe_new(a: number, b: number, c: number): number;
+export function universe_render(a: number, b: number): void;
+export function add(a: number, b: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
